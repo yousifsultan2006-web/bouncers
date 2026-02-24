67a7bd055846f7f06053122b9e3c2598f49077f9
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-02-23 20:55:31 -0800

    refactored the average function to use Bouncer, added y axis movements

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,11 @@
 
 // Set max/min x position to be the edges of the display
 static constexpr int HALF_SCREEN_WIDTH = bn::display::width() / 2;
+static constexpr int HALF_SCREEN_HEIGHT = bn::display::height() / 2;
 static constexpr bn::fixed MIN_X = -HALF_SCREEN_WIDTH;
 static constexpr bn::fixed MAX_X = HALF_SCREEN_WIDTH;
+static constexpr bn:: fixed MIN_Y = -HALF_SCREEN_HEIGHT;
+static constexpr bn::fixed MAX_Y = HALF_SCREEN_HEIGHT;
 
 // Starting speed of a bouncer
 static constexpr bn::fixed BASE_SPEED = 2;
@@ -24,43 +27,57 @@ class Bouncer {
 public:
     bn::sprite_ptr sprite = bn::sprite_items::dot.create_sprite();
     bn::fixed x_speed = BASE_SPEED;
+    bn::fixed y_speed = BASE_SPEED;
 
     void update() {
         
             
-            
+            bn::fixed y = sprite.y();
 
             bn::fixed x = sprite.x();
 
             // Update x position by adding speed
             x += x_speed;
+            y += y_speed;
 
-            // If we've gone off the screen on the right
+            // If we've gone off the screen on the right (for x)
             if(x > MAX_X) {
                 // Snap back to screen and reverse direction
                 x = MAX_X;
                 x_speed *=-1;
             }
-            // If we've gone off the screen on the left
+            // If we've gone off the screen on the left (for x)
             if(x < MIN_X) {
                 // Snap back to screen and reverse direction
                 x = MIN_X;
                 x_speed *= -1;
             }
+            // if we've gone off the screen on the bottom (for y)
+            if(y> MAX_Y) {
+                // Snap back to screen and reverse direction
+                y = MAX_Y;
+                y_speed *= -1;
+            }
+            // if we've gone off the screen on the top (for y)
+            if(y < MIN_Y) {
+                // Snap back to screen and reverse direction
+                y = MIN_Y;
+                y_speed *= -1;
+            }
 
             sprite.set_x(x);
+            sprite.set_y(y);
         
-
     }
 
 };
 
 
             
-bn::fixed average_x(bn::vector<bn::sprite_ptr, MAX_BOUNCERS>& sprites) {
+bn::fixed average_x(bn::vector<Bouncer, MAX_BOUNCERS>& bouncers) {
     bn::fixed x_sum = 0;
-    for(bn::sprite_ptr sprite : sprites) {
-        x_sum /= sprites.size();
+    for(Bouncer bouncer : bouncers) {
+        x_sum += bouncer.sprite.x();
     }
     return x_sum;
 
